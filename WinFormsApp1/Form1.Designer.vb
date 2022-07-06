40c11f964b01fe7ea1227f8609c2e45dee237dba
enriquesalomon <king92salomon@gmail.com>
2022-07-06 23:59:55 +0800

    Firs Commit

--- WinFormsApp1/Form1.Designer.vb
+++ WinFormsApp1/Form1.Designer.vb
@@ -25,38 +25,82 @@ Partial Class Form1
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.TEXT1ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.TEXT2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PAIDToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UNPAIDToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.REMITTANCEWITHBALANCEToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RIDERSREMITTANCEToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VIEWUNPAIDREMITTANCEToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.PanelLogin = New System.Windows.Forms.Panel()
-        Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.PanelBottom = New System.Windows.Forms.Panel()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
+        Me.PanelLogin = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PanelBottom = New System.Windows.Forms.Panel()
         Me.MenuStrip1.SuspendLayout()
         Me.Panel1.SuspendLayout()
         Me.PanelLogin.SuspendLayout()
+        Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
         '
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TEXT1ToolStripMenuItem, Me.TEXT2ToolStripMenuItem})
+        Me.MenuStrip1.BackColor = System.Drawing.Color.Transparent
+        Me.MenuStrip1.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TEXT1ToolStripMenuItem, Me.TEXT2ToolStripMenuItem, Me.RIDERSREMITTANCEToolStripMenuItem, Me.VIEWUNPAIDREMITTANCEToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 31)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(1253, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(1253, 25)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
         'TEXT1ToolStripMenuItem
         '
         Me.TEXT1ToolStripMenuItem.Name = "TEXT1ToolStripMenuItem"
-        Me.TEXT1ToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
-        Me.TEXT1ToolStripMenuItem.Text = "TEXT 1"
+        Me.TEXT1ToolStripMenuItem.Size = New System.Drawing.Size(194, 21)
+        Me.TEXT1ToolStripMenuItem.Text = "GENERATE RECEIVERS FORM"
         '
         'TEXT2ToolStripMenuItem
         '
+        Me.TEXT2ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PAIDToolStripMenuItem, Me.UNPAIDToolStripMenuItem, Me.REMITTANCEWITHBALANCEToolStripMenuItem})
         Me.TEXT2ToolStripMenuItem.Name = "TEXT2ToolStripMenuItem"
-        Me.TEXT2ToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
-        Me.TEXT2ToolStripMenuItem.Text = "TEXT 2"
+        Me.TEXT2ToolStripMenuItem.Size = New System.Drawing.Size(96, 21)
+        Me.TEXT2ToolStripMenuItem.Text = "REMITTANCE"
+        '
+        'PAIDToolStripMenuItem
+        '
+        Me.PAIDToolStripMenuItem.Name = "PAIDToolStripMenuItem"
+        Me.PAIDToolStripMenuItem.Size = New System.Drawing.Size(249, 22)
+        Me.PAIDToolStripMenuItem.Text = "PAID"
+        '
+        'UNPAIDToolStripMenuItem
+        '
+        Me.UNPAIDToolStripMenuItem.Name = "UNPAIDToolStripMenuItem"
+        Me.UNPAIDToolStripMenuItem.Size = New System.Drawing.Size(249, 22)
+        Me.UNPAIDToolStripMenuItem.Text = "UNPAID"
+        '
+        'REMITTANCEWITHBALANCEToolStripMenuItem
+        '
+        Me.REMITTANCEWITHBALANCEToolStripMenuItem.Name = "REMITTANCEWITHBALANCEToolStripMenuItem"
+        Me.REMITTANCEWITHBALANCEToolStripMenuItem.Size = New System.Drawing.Size(249, 22)
+        Me.REMITTANCEWITHBALANCEToolStripMenuItem.Text = "REMITTANCE WITH BALANCE"
+        '
+        'RIDERSREMITTANCEToolStripMenuItem
+        '
+        Me.RIDERSREMITTANCEToolStripMenuItem.Name = "RIDERSREMITTANCEToolStripMenuItem"
+        Me.RIDERSREMITTANCEToolStripMenuItem.Size = New System.Drawing.Size(142, 21)
+        Me.RIDERSREMITTANCEToolStripMenuItem.Text = "RIDERS REMITTANCE"
+        '
+        'VIEWUNPAIDREMITTANCEToolStripMenuItem
+        '
+        Me.VIEWUNPAIDREMITTANCEToolStripMenuItem.Name = "VIEWUNPAIDREMITTANCEToolStripMenuItem"
+        Me.VIEWUNPAIDREMITTANCEToolStripMenuItem.Size = New System.Drawing.Size(76, 21)
+        Me.VIEWUNPAIDREMITTANCEToolStripMenuItem.Text = "LOG-OUT"
         '
         'Panel1
         '
@@ -68,51 +112,6 @@ Partial Class Form1
         Me.Panel1.Size = New System.Drawing.Size(1253, 31)
         Me.Panel1.TabIndex = 1
         '
-        'PanelLogin
-        '
-        Me.PanelLogin.Controls.Add(Me.Button2)
-        Me.PanelLogin.Controls.Add(Me.Button1)
-        Me.PanelLogin.Controls.Add(Me.Panel2)
-        Me.PanelLogin.Location = New System.Drawing.Point(374, 198)
-        Me.PanelLogin.Name = "PanelLogin"
-        Me.PanelLogin.Size = New System.Drawing.Size(521, 156)
-        Me.PanelLogin.TabIndex = 2
-        '
-        'Panel2
-        '
-        Me.Panel2.BackColor = System.Drawing.Color.Goldenrod
-        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Top
-        Me.Panel2.Location = New System.Drawing.Point(0, 0)
-        Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(521, 37)
-        Me.Panel2.TabIndex = 0
-        '
-        'PanelBottom
-        '
-        Me.PanelBottom.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.PanelBottom.Location = New System.Drawing.Point(0, 716)
-        Me.PanelBottom.Name = "PanelBottom"
-        Me.PanelBottom.Size = New System.Drawing.Size(1253, 33)
-        Me.PanelBottom.TabIndex = 3
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(313, 117)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 32)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(411, 117)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 32)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
         'Button3
         '
         Me.Button3.Dock = System.Windows.Forms.DockStyle.Right
@@ -123,10 +122,115 @@ Partial Class Form1
         Me.Button3.Text = "X"
         Me.Button3.UseVisualStyleBackColor = True
         '
+        'PanelLogin
+        '
+        Me.PanelLogin.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.PanelLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PanelLogin.Controls.Add(Me.Label2)
+        Me.PanelLogin.Controls.Add(Me.Label1)
+        Me.PanelLogin.Controls.Add(Me.TextBox2)
+        Me.PanelLogin.Controls.Add(Me.TextBox1)
+        Me.PanelLogin.Controls.Add(Me.Button2)
+        Me.PanelLogin.Controls.Add(Me.Button1)
+        Me.PanelLogin.Controls.Add(Me.Panel2)
+        Me.PanelLogin.Location = New System.Drawing.Point(374, 198)
+        Me.PanelLogin.Name = "PanelLogin"
+        Me.PanelLogin.Size = New System.Drawing.Size(411, 139)
+        Me.PanelLogin.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Goldenrod
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(109, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 21)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Password   :"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Goldenrod
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(109, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 21)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Username :"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(205, 72)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(195, 23)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(205, 43)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(195, 23)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(312, 107)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(88, 29)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Close"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(218, 107)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(88, 29)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Login"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Goldenrod
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel2.Location = New System.Drawing.Point(0, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(409, 27)
+        Me.Panel2.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Goldenrod
+        Me.Label3.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(2, 4)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(118, 16)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "LOGIN ACCOUNT"
+        '
+        'PanelBottom
+        '
+        Me.PanelBottom.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.PanelBottom.Location = New System.Drawing.Point(0, 716)
+        Me.PanelBottom.Name = "PanelBottom"
+        Me.PanelBottom.Size = New System.Drawing.Size(1253, 33)
+        Me.PanelBottom.TabIndex = 3
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(1253, 749)
         Me.Controls.Add(Me.PanelBottom)
         Me.Controls.Add(Me.PanelLogin)
@@ -141,6 +245,9 @@ Partial Class Form1
         Me.MenuStrip1.PerformLayout()
         Me.Panel1.ResumeLayout(False)
         Me.PanelLogin.ResumeLayout(False)
+        Me.PanelLogin.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -156,4 +263,14 @@ Partial Class Form1
     Friend WithEvents Button3 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Button1 As Button
+    Friend WithEvents PAIDToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UNPAIDToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents REMITTANCEWITHBALANCEToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RIDERSREMITTANCEToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents VIEWUNPAIDREMITTANCEToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
 End Class
--- WinFormsApp1/Form1.vb
+++ WinFormsApp1/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Me.Close()
     End Sub
+
+    Private Sub TEXT1ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TEXT1ToolStripMenuItem.Click
+
+    End Sub
 End Class
